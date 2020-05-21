dd955a6bc6288f2346499bcfe5211c96878f5cb9
Jon Baltz <baltz.jon.r@gmail.com>
2020-05-21 17:47:51 -0600

    Complete fibonacciNumber problem

---
+++ fibonacciNumber.js
@@ -0,0 +1,17 @@
+const fib = function(n) {
+	if (n < 2) {
+		return n;
+	}
+	let x = 0;
+	let y = 1;
+	for (let i = 2; i <= n; i++) {
+		let next = x + y;
+		x = y;
+		y = next;
+	}
+	return y;
+}
+
+console.assert(fib(0) === 0, "works for 0");
+console.assert(fib(1) === 1, "works for 1");
+console.assert(fib(3) === 2, "works for 3");
