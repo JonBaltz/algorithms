7595455d69682af2394a6f0b45aebb52feb1d6cd
Jon Baltz <baltz.jon.r@gmail.com>
2020-05-23 14:27:54 -0600

    Complete invertBinaryTree problem

--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -1,7 +1,13 @@
 // https://leetcode.com/problems/invert-binary-tree/
 
 const invertTree = function(root) {
-	//
+	if (!root) {
+		return null;
+	}
+	const temp = invertTree(root.left);
+	root.left = invertTree(root.right);
+	root.right = temp;
+	return root;
 }
 
 const TreeNode = function(val) {
@@ -11,11 +17,11 @@ const TreeNode = function(val) {
 }
 
 const tree = new TreeNode(1);
-const invertedTree = new Treenode(1);
+const invertedTree = new TreeNode(1);
 console.assert(JSON.stringify(invertTree(tree)) === JSON.stringify(invertedTree), "doesn't change a single node");
 tree.left = new TreeNode(2);
 tree.right = new TreeNode(3);
 invertedTree.left = new TreeNode(3);
 invertedTree.right = new TreeNode(2);
-console.assert(JSON.stringify(invertTree(tree)) === JSON.Stringify(invertedTree), "inverts a tree");
+console.assert(JSON.stringify(invertTree(tree)) === JSON.stringify(invertedTree), "inverts a tree");
 
--- sortArrayByParity.js
+++ sortArrayByParity.js
@@ -20,12 +20,9 @@ const sortArrayByParity = function (A) {
     return A;
 };
 
-console.time("bruh");
 console.log("hello");
 console.assert(JSON.stringify(sortArrayByParity([3, 1, 2, 4])) === JSON.stringify([4, 2, 1, 3]), "works");
 console.assert(JSON.stringify(sortArrayByParity([1, 4, 3, 5, 2, 6])) === JSON.stringify([6, 4, 2, 5, 3, 1]), "works");
 console.assert(JSON.stringify(sortArrayByParity([1, 3, 5, 7, 9])) === JSON.stringify([1, 3, 5, 7, 9]), "ends correctly");
 const odds = [1, 3, 5, 7, 9];
 console.assert(sortArrayByParity(odds) === odds, "doesn't mutate when it doesn't have to");
-console.timeLog("bruh");
-console.timeEnd("bruh");
