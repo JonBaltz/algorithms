21fbfce8ec0b3c00a44653ea5eedb89584b353da
Jon Baltz <baltz.jon.r@gmail.com>
2020-05-26 10:54:04 -0600

    Optimized findDisappearedNumbers solution

--- findDisappearedNumbers.js
+++ findDisappearedNumbers.js
@@ -1,20 +1,31 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 const findDisappearedNumbers = function(array) {
-	let unseen = [];
-	for (let i = 1; i <= array.length; i++) {
-		unseen.push(i);
-	}
-	for (let i = 0; i < array.length; i++) {
-		unseen[array[i] - 1] = 0;
-	}
-	let result = [];
-	for (let i = 0; i < unseen.length; i++) {
-		if (unseen[i]) {
-			result.push(unseen[i]);
+	let index = 0;
+	let temp;
+	while (index < array.length) {
+		let curr = array[index];
+		if (curr !== index + 1) {
+			let target = array[curr - 1];
+			if (curr !== target) {
+				temp = array[index];	
+				array[index] = array[curr - 1];
+				array[curr - 1] = temp;
+			} else {
+				index++;
+			}
+		} else {
+			index++;
 		}
 	}
-	return result;
+	index = 0;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] !== i + 1) {
+			array[index++] = i + 1;
+		}
+	}
+	array.length = index;
+	return array;
 }
 
 console.assert(JSON.stringify(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])) === JSON.stringify([5, 6]), "works", findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
