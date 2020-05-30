68d0ec6500b98c9d30ebd30774808d6bbc6b25ed
Jon Baltz <baltz.jon.r@gmail.com>
2020-05-30 12:55:53 -0600

    Complete shuffle Deck problem

---
+++ shuffleDeck.js
@@ -0,0 +1,21 @@
+const shuffleDeck = function(deck) {
+	for (let i = 0; i < deck.length; i++) {
+		let random = Math.floor(Math.random() * deck.length - i) + i;
+		let temp = deck[random];
+		deck[random] = deck[i];
+		deck[i] = temp;
+	}
+	return deck;
+}
+
+const orderedDeck = function() {
+	const suits = [ '♥', '♣', '♠', '♦' ];
+	const values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+	const deck = [];
+	suits.forEach((suit) => values.forEach((value) => {deck.push(value + suit);}));
+	return deck;
+}
+
+const deck = orderedDeck();
+console.log(deck);
+console.log(shuffleDeck(deck));
