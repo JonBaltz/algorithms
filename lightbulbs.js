6f8f797c9a618a75779054067f81e86fdcc163d9
Jon Baltz <baltz.jon.r@gmail.com>
2020-06-03 12:24:29 -0600

    Complete lightbulb problem

---
+++ lightbulbs.js
@@ -0,0 +1,7 @@
+const howManyOn = function(n) {
+	return Math.floor(Math.sqrt(n));
+}
+
+console.assert(howManyOn(1) === 1, "works for one");
+console.assert(howManyOn(6) === 2, "works for six");
+console.assert(howManyOn(30) === 5, "works for five");
