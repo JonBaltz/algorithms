499838ff5d6436c78cd1eb6590246343b2b666c4
Jon Baltz <baltz.jon.r@gmail.com>
2020-06-10 15:41:45 -0600

    Add edge-case handlers for searchInsertPosition problem

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/search-insert-position/
 
 const searchInsert = function(nums, target) {
+	if (target < nums[0]) return 0;
+	if (target > nums[nums.length - 1]) return nums.length;
 	let start = 0;
 	let end = nums.length - 1;
 	let mid;
