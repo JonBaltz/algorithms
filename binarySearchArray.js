7ba4f1581e1559f3a3ec2006925cf4d0900a86cb
Jon Baltz <baltz.jon.r@gmail.com>
2020-06-10 14:02:48 -0600

    Add more tests to binarySearchArray

--- binarySearchArray.js
+++ binarySearchArray.js
@@ -16,6 +16,8 @@ const binarySearch = function(array, target) {
 console.assert(binarySearch([1, 2, 3, 4, 5], 3) === 2, "finds the middle item");
 console.assert(binarySearch([1, 2, 3, 4, 5], 2) === 1, "finds 2");
 console.assert(binarySearch([1, 2, 3, 4, 5], 1) === 0, "finds 1");
+console.assert(binarySearch([1, 2, 3, 4, 5], 4) === 3, "finds 4");
+console.assert(binarySearch([1, 2, 3, 4, 5], 5) === 4, "finds 5");
 console.assert(binarySearch([1, 2, 3, 4, 5], 6) === null, "returns null for a missing item");
 console.assert(binarySearch([1, 2, 3, 4, 5], 0) === null, "returns null for a missing item");
 console.assert(binarySearch([1, 2, 3, 4], 4) === 3, "works for a starting even array");
