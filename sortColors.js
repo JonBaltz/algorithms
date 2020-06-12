7b2468a481d6c867b920ce1b4305272d49baa2a1
Jon Baltz <baltz.jon.r@gmail.com>
2020-06-11 19:15:11 -0600

    Optimize the solution of sortColors to use one pass

--- sortColors.js
+++ sortColors.js
@@ -1,24 +1,28 @@
 // https://leetcode.com/problems/sort-colors/
 
 const sortColors = function(nums) {
-	const c = {"0": 0, "1": 0, "2": 0};
-	for (let i = 0; i < nums.length; i++) {
-		c[nums[i]]++;
+	let s = 0;
+	let e = nums.length - 1;
+	while (nums[s] === 0) {
+		s++;
 	}
-	for (let i = 0; i < nums.length; i++) {
-		if (c["0"]) {
-			nums[i] = 0;
-			c["0"]--;
-		} else if (c["1"]) {
-			nums[i] = 1;
-			c["1"]--;
+	let i = s;
+	while (i <= e) {
+		if (nums[i] === 0) {
+			nums[i++] = nums[s];
+			nums[s++] = 0;
+		} else if (nums[i] === 2) {
+			nums[i] = nums[e];
+			nums[e--] = 2;
 		} else {
-			nums[i] = 2;
+			i++;
 		}
 	}
 	return nums;
 };
 
 console.assert(JSON.stringify(sortColors([1, 2, 1, 0, 0, 2, 1])) === JSON.stringify([0, 0, 1, 1, 1, 2, 2]), "works for normal example");
+console.assert(JSON.stringify(sortColors([2, 0, 2, 1, 1, 0])) == JSON.stringify([0, 0, 1, 1, 2, 2]), "works for leetcode example");
+console.log(sortColors([2, 0, 2, 1, 1, 0]));
 console.assert(JSON.stringify(sortColors([0, 0, 0, 0])) === JSON.stringify([0, 0, 0, 0]), "works for all 0s");
 console.assert(JSON.stringify(sortColors([2, 2, 2, 2])) === JSON.stringify([2, 2, 2, 2]), "works for all 2s");
