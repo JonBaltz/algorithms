d238d3d2d704bbf1f0043ce353ea040ede91f36d
Jon Baltz <baltz.jon.r@gmail.com>
2020-07-28 21:45:00 -0600

    Complete charachetrFrequency toy problem

---
+++ characterFrequency.js
@@ -0,0 +1,24 @@
+const characterFrequency = function (str) {
+	const counts = {};
+	for (let i = 0; i < str.length; i++) {
+		if (!counts[str[i]]) {
+			counts[str[i]] = 1;
+		} else {
+			counts[str[i]]++;
+		}
+	}
+	const result = Object.entries(counts);
+	result.sort((a, b) => {
+		if (a[0] > b[0]) {
+			return 1;
+		} else {
+			return -1;
+		}
+	});
+	result.sort((a, b) => b[1] - a[1]);
+	return result;
+};
+
+console.log(characterFrequency("mississippi"));
+console.log(characterFrequency("miaaiaaippi"));
+console.log(characterFrequency("mmmaaaiiibbblll"));
