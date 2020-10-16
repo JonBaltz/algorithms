0ae2233a5fac9e030a6dbb0534ce34140a4c9e87
Jon Baltz <baltz.jon.r@gmail.com>
2020-10-15 23:51:22 -0600

    Complete move zeros problem in python

---
+++ moveZeroes.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/move-zeroes/
+
+class Solution:
+	def moveZeros(self, nums):
+		dex = 0
+		for i, val in enumerate(nums):
+			if val != 0:
+				nums[dex] = val
+				nums[i] = 0
+				dex += 1
+		print(nums)
+
+test = Solution()
+
+test.moveZeros([0, 1, 0, 3, 0, 0, 10])
