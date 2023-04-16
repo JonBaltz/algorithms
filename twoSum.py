a82506bb6a01d47c309b627d4c272feeb379d693
Jon Baltz <baltz.jon.r@gmail.com>
2023-04-16 09:56:17 -0600

    twoSum in python

--- twoSum.py
+++ twoSum.py
@@ -1,13 +1,37 @@
 # https://leetcode.com/problems/two-sum/
 
-class Solution:
-	def twoSum(self, nums, target):
-		hash = {}
-		for i, num in enumerate(nums):
-			if num in hash:
-				return [hash[num], i]
-			else:
-				hash[target - num] = i
+def twoSumExtraSpace(nums, target):
+	map = {}
 
-test = Solution()
-print(test.twoSum([2, 7, 11, 20], 9))
+	for index, value in enumerate(nums):
+		if target - value in map:
+			return (map[target - value], index)
+		else:
+			map[value] = index
+
+
+def twoSum(nums, target):
+	low, high = 0, len(nums) - 1
+
+	while low < high:
+		current = nums[low] + nums[high]
+
+		if current == target:
+			return (low, high)
+
+		if current < target:
+			low += 1
+		else: 
+			high -= 1
+	
+	return None
+
+
+print(twoSum([2, 7, 11, 20], 9), (0, 1))
+print(twoSum([2, 7, 11, 20], 31), (2, 3))
+print(twoSum([2, 7, 11, 20], 22), (0, 3))
+print(twoSum([2, 7, 11, 20], 18), (1, 2))
+print(twoSumExtraSpace([2, 7, 11, 20], 9), (0, 1))
+print(twoSumExtraSpace([2, 7, 11, 20], 31), (2, 3))
+print(twoSumExtraSpace([2, 7, 11, 20], 22), (0, 3))
+print(twoSumExtraSpace([2, 7, 11, 20], 18), (1, 2))
